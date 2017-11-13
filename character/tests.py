c59aca8df31db74b802ea5607c93c596a1e089cf
Dougal Scott <dougal.scott@gmail.com>
2017-11-14 09:33:11 +1100

    Added some tests

--- character/tests.py
+++ character/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
-# Create your tests here.
+from character.models import Thief
+
+
+class test_Thief(TestCase):
+    def setUp(self):
+        self.th = Thief(name='test')
+        self.th.save()
+
+    def test_hp(self):
+        self.assertEqual(self.th.hitdie, 6)
+        self.assertGreaterEqual(self.th.hp, 1)
+        self.assertLessEqual(self.th.hp, 6)
+
+    def test_thaco(self):
+        self.assertEqual(self.th.thaco, 20)
+
+    def test_encumb(self):
+        self.assertEqual(self.th.encumbrance, 0)
+
+    def test_stats(self):
+        self.assertGreaterEqual(self.th.stat_dex, 3)
+        self.assertLessEqual(self.th.stat_dex, 18)
+
+
+# EOF
