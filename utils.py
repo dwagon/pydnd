e0d73d6378996bf099b8f86799a568f3590daf65
Dougal Scott <dougal.scott@gmail.com>
2017-11-17 18:44:33 +1100

    Handle both sorts of answers

--- utils.py
+++ utils.py
@@ -3,6 +3,12 @@ import dice
 
 def roll(dstr):
     """ Convert something like 1d6+3 into an appropriate random number """
-    return sum(dice.roll(dstr))
+    d = dice.roll(dstr)
+    # Under some circumstances the roll returns an int otherwise an iterable object
+    try:
+        return sum(d)
+    except TypeError:
+        return d
+
 
 # EOF
