71f45867fc68c0a3801a41f02e9cefc7d2c86526
Dougal Scott <dougal.scott@gmail.com>
2017-11-24 18:05:22 +1100

    Some more encounter tests

--- world/tests/encounter.py
+++ world/tests/encounter.py
@@ -25,6 +25,7 @@ class test_Encounter(TestCase):
         figster = Character.objects.get(world=self.w, name='Figgy')
         self.assertEqual(figster.x, 10)
         self.assertEqual(figster.y, 10)
+        self.assertEqual(e.arena[(10, 10)], figster)
 
     def test_objtype(self):
         e = Encounter(world=self.w, monstername='TestOrc')
@@ -37,5 +38,26 @@ class test_Encounter(TestCase):
         ans = e.objtype(mi)
         self.assertEqual(ans, Encounter.MONSTER)
 
+    def test_place_pcs(self):
+        """ Test PC placement in arena """
+        for i in range(10):
+            f = Fighter(name='F{}'.format(i), world=self.w)
+            f.generate_stats()
+            f.save()
+        e = Encounter(world=self.w, monstername='TestOrc', arenasize=20)
+        e.place_pcs()
+        used_locs = e.arena.keys()
+        pc_locs = [(_.x, _.y) for _ in Character.objects.filter(world=self.w)]
+        self.assertEquals(sorted(used_locs), sorted(pc_locs))
+
+    def test_place_monsters(self):
+        """ Test Monster placement in arena """
+        m = Monster(name='TestManyOrc', ac=19, xp=5, thaco=19, movement=3, numappearing='25')
+        m.save()
+        e = Encounter(world=self.w, monstername='TestManyOrc', arenasize=10)
+        e.place_monsters()
+        used_locs = e.arena.keys()
+        m_locs = [(_.x, _.y) for _ in e.monsters]
+        self.assertEquals(sorted(used_locs), sorted(m_locs))
 
 # EOF
