ff637efd854f55c0de76971ce1b345c056a1a3f6
Dougal Scott <dougal.scott@gmail.com>
2017-11-27 09:25:45 +1100

    Retrieve frontend content from backend

---
+++ world/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^map/(?P<level>\S+)', views.levelmap),
+    url(r'^tileset/(?P<tiles>\S+)', views.tileset),
+    url(r'^tile/(?P<tilename>\S+)', views.tile)
+]
--- world/views.py
+++ world/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+import os
+import json
 
-# Create your views here.
+
+##############################################################################
+def levelmap(request, level):
+    mapdir = os.path.join('world', 'maps', level)
+    f = open(os.path.join(mapdir, 'map.txt'))
+    m = f.read().split('\n')
+    f.close()
+
+    g = open(os.path.join(mapdir, 'tileset.json'))
+    lmap = json.load(g)
+    lmap["map"] = m
+    return JsonResponse(lmap)
+
+
+##############################################################################
+def tileset(request, tiles):
+    f = open(os.path.join('world', 'tileset', "{}.json".format(tiles)))
+    d = json.load(f)
+    return JsonResponse(d)
+
+
+##############################################################################
+def tile(request, tilename):
+    imgfile = os.path.join('world', 'tiles', "{}.png".format(tilename))
+    with open(imgfile, "rb") as f:
+        return HttpResponse(f.read(), content_type="image/png")
+
+# EOF
