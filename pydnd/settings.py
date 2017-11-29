65e5bb6c8be7029987d482e0c7c571ef3f5c7dbf
Dougal Scott <dougal.scott@gmail.com>
2017-11-30 08:06:17 +1030

    Moved to a better dir structure

--- pydnd/settings.py
+++ pydnd/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Look in the lib dir for stuff
+sys.path.insert(0, os.path.join(BASE_DIR, "lib"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
