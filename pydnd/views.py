05821cc364e7a2ea4d1df079c124c1454d9651f2
Dougal Scott <dougal.scott@gmail.com>
2017-12-09 11:41:09 +1100

    Top level URL for rest

---
+++ pydnd/views.py
@@ -0,0 +1,19 @@
+from rest_framework import permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
+def api_root(request, format=None):
+    return Response({
+        # 'monster': reverse('monster-list', request=request, format=format),
+        # 'monsterstate': reverse('monsterstate-list', request=request, format=format),
+        'character': reverse('character-list', request=request, format=format),
+        # 'encounter': reverse('encounter-list', request=request, format=format),
+        'equipment': reverse('equipment-list', request=request, format=format),
+        'world': reverse('world-list', request=request, format=format)
+    })
+
+# EOF
