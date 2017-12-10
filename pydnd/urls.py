405e900e41ef45f880c02ce18dc9d9e04758ad37
Dougal Scott <dougal.scott@gmail.com>
2017-12-11 09:32:53 +1100

    Expose encounter API

--- pydnd/urls.py
+++ pydnd/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^$', views.api_root),
     url(r'^admin/', admin.site.urls),
     url(r'^world/', include('world.urls')),
+    url(r'^encounter/', include('encounter.urls')),
     url(r'^equipment/', include('equipment.urls')),
     url(r'^character/', include('character.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
