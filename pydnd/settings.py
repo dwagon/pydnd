d0dce2adb9524f4b370d85351a01a77cc55bd0d3
Dougal Scott <Dougal.Scott@gmail.com>
2017-12-10 15:53:04 +1100

    Use sqlite as it is LCD

--- pydnd/settings.py
+++ pydnd/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'pydnd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'pydnd',
         'USER': 'pydnd',
         'PASSWORD': 'pydnd',
