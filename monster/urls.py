b5e600ee9f3805c3b4dc40570e407dd78cba7e18
Dougal Scott <dougal.scott@gmail.com>
2018-02-08 17:31:05 +1100

    API access to monster state

--- monster/urls.py
+++ monster/urls.py
@@ -4,7 +4,9 @@ from monster import views
 
 urlpatterns = [
     url(r'^$', views.MonsterList.as_view(), name='monster-list'),
-    url(r'^(?P<pk>[0-9]+)/$', views.MonsterDetail.as_view(), name='monster-detail')
+    url(r'^(?P<pk>[0-9]+)/$', views.MonsterDetail.as_view(), name='monster-detail'),
+    url(r'^state/$', views.MonsterStateList.as_view(), name='monsterstate-list'),
+    url(r'^state/(?P<pk>[0-9]+)/$', views.MonsterStateDetail.as_view(), name='monsterstate-detail')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- monster/views.py
+++ monster/views.py
@@ -1,5 +1,5 @@
-from .models import Monster
-from .serializers import MonsterSerializer
+from .models import Monster, MonsterState
+from .serializers import MonsterSerializer, MonsterStateSerializer
 from rest_framework import generics
 
 
@@ -21,6 +21,26 @@ class MonsterList(generics.ListCreateAPIView):
 class MonsterDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Monster.objects.all()
     serializer_class = MonsterSerializer
+
+
+##############################################################################
+class MonsterStateList(generics.ListCreateAPIView):
+    serializer_class = MonsterStateSerializer
+
+    def get_queryset(self):
+        queryset = MonsterState.objects.all()
+
+        enc = self.request.query_params.get('encounter', None)
+        if enc is not None:
+            queryset = queryset.filter(encounter=enc)
+
+        return queryset
+
+
+##############################################################################
+class MonsterStateDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MonsterState.objects.all()
+    serializer_class = MonsterStateSerializer
 
 
 # EOF
