2826a274031dacaaa5d10280f511c1a097148fe8
Dougal Scott <dougal.scott@gmail.com>
2018-02-19 16:02:05 +1100

    Indicate end of phase

--- frontend/curses_fe.py
+++ frontend/curses_fe.py
@@ -16,6 +16,8 @@ class Screen(object):
         self.screen_height, self.screen_width = screen.getmaxyx()
         self.arena_x = int((self.screen_width * 2)/(3 * 5))
         self.arena_y = int((self.screen_height - msg_win_height + 2) / 2) - 3
+        self.phase = 'unknown'
+        self.turn = 'unknown'
         self.init_windows()
         self.encounter_id = self.init_game()
 
@@ -39,6 +41,7 @@ class Screen(object):
     def init_game(self):
         pydnd.initiate_session()
         world_id = pydnd.get_world()
+        pydnd.delete_all_chars()
         self.add_msg('Making Chars', refresh=True)
         pydnd.make_thief(world_id, "Tom")
         pydnd.make_fighter(world_id, "Fin")
@@ -63,15 +66,24 @@ class Screen(object):
         if self.encounter_id:
             msgs = pydnd.get_messages(self.encounter_id, max_num=msg_win_height, delete=True)
             self.msgbuf.extend(msgs)
+            self.msgbuf.append("--- Press Return ---")
         self.message_win.erase()
         self.message_win.border()
+        self.message_win.addstr(0, 1, " Turn: {} Phase {} ".format(self.turn, self.phase))
         for num, mesg in enumerate(self.msgbuf):
             self.message_win.addstr(num+1, 1, mesg)
         self.message_win.refresh()
 
     #########################################################################
+    def get_encounter(self):
+        enc = pydnd.get_encounter(self.encounter_id)
+        self.phase = enc['phase']
+        self.turn = enc['turn']
+
+    #########################################################################
     def loop(self):
         while True:
+            self.get_encounter()
             self.draw_map(self.map_win, self.encounter_id)
             finished = pydnd.combat_phase(self.encounter_id)
             self.display_messages()
@@ -84,18 +96,14 @@ class Screen(object):
     def draw_map(self, win, eid):
         win.clear()
         win.border()
-        f = open('/tmp/c.err', 'w')
-        f.write("Arena = {} {}\n".format(self.arena_y, self.arena_x))
         arena = pydnd.get_arena(eid)
         for x in range(self.arena_x):
             for y in range(self.arena_y):
-                f.write("{} {} = {} {} ({})\n".format(y, x, y*2+1, x*5+1, win.getmaxyx()))
                 win.addstr(y*2+1, x*5+1, '.')
         for loc in arena:
             x_str, y_str = loc.split()
             x, y = int(x_str), int(y_str)
             win.addnstr(y*2+1, x*5+1, arena[loc]['content']['name'], 5)
-        f.close()
         win.refresh()
 
 
