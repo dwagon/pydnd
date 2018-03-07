030b56fd4023d70273b9e44a086b1387510ee91d
Dougal Scott <dougal.scott@gmail.com>
2018-03-07 20:12:21 +1100

    Default for properties

--- equipment/models/weapon.py
+++ equipment/models/weapon.py
@@ -14,7 +14,7 @@ class Weapon(Equipment):
         )
     damage = models.CharField(max_length=10)
     damage_cat = models.CharField(max_length=2, choices=damage_cat_choices)
-    properties = models.CharField(max_length=50)
+    properties = models.CharField(max_length=50, default='')
     normal_range = models.IntegerField(default=0)
     long_range = models.IntegerField(default=0)
 
